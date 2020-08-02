6226565df2c1943f27e839ab898d5701383ec8f7
rajnavakoti <rajnavakoti@gmail.com>
2020-08-02 14:33:33 +0200

    base stepup file for UI test cases

--- src/main/groovy/com/yahoofinance/utils/configuration/Config.groovy
+++ src/main/groovy/com/yahoofinance/utils/configuration/Config.groovy
@@ -20,4 +20,28 @@ class Config {
         if (getEnvironment().equals('prod') || getEnvironment().equals('production'))
             getKey("YahooFinanaceApiUrl", getEnvironment())
     }
+
+    static getBrowser() {
+        return getKey("browser")
+    }
+
+    static getHeadless() {
+        return getKey("headless")
+    }
+
+    static getBrowserWidth() {
+        return getKey("browserWidth")
+    }
+
+    static getBrowserLength() {
+        return getKey("browserLength")
+    }
+
+    static getFailScreenshotsPath() {
+        return getKey("FailScreenShotPath")
+    }
+
+    static boolean isJenkins() {
+        return getKey("executionEnvironment") == 'jenkins'
+    }
 }
